0480c21631b4bf12f108206d7cbb39eb0c38250b
zhouliang <dear.zhouliang@qq.com>
2025-06-05 16:44:30 +0800

    feat: ctx中增加permissionRegionIds

--- api/internal/logic/publicuser/login_logic.go
+++ api/internal/logic/publicuser/login_logic.go
@@ -39,83 +39,99 @@ func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginResp, err erro
 		return nil, errorx.NewCodeAbortedError("login.loginTypeForbidden")
 	}
 
-	if ok := l.svcCtx.Captcha.Verify(config.RedisCaptchaPrefix+req.CaptchaId, req.Captcha, true); ok {
-		user, err := l.svcCtx.CoreRpc.GetUserByUsername(l.ctx,
-			&core.UsernameReq{
-				Username: req.Username,
-			})
-		if err != nil {
-			return nil, err
-		}
-
-		if user.Status != nil && *user.Status != uint32(common.StatusNormal) {
-			return nil, errorx.NewCodeInvalidArgumentError("login.userBanned")
-		}
-
-		password, err := l.svcCtx.Sm2.Sm2Decrypt(req.Password)
-		if err != nil {
-			return nil, err
-		}
-
-		if !encrypt.BcryptCheck(password, *user.Password) {
-			return nil, errorx.NewCodeInvalidArgumentError("login.wrongUsernameOrPassword")
-		}
-		// 获取 position id
-		userData, err := l.svcCtx.CoreRpc.GetUserList(l.ctx, &core.UserListReq{
-			UsernameStrict: &req.Username,
-		})
-		if err != nil {
-			return nil, err
-		}
-		// 将整数切片转换为字符串切片
-		var stringSlice []string
-		for _, num := range userData.Data[0].PositionIds {
-			stringSlice = append(stringSlice, strconv.Itoa(int(num)))
-		}
-		// 使用 strings.Join 将字符串切片转换为逗号隔开的字符串
-		positionIds := strings.Join(stringSlice, ",")
-
-		token, err := jwt.NewJwtToken(
-			l.svcCtx.Config.Auth.AccessSecret, time.Now().Unix(),
-			l.svcCtx.Config.Auth.AccessExpire, jwt.WithOption("userId", user.Id),
-			jwt.WithOption("roleId", strings.Join(user.RoleCodes, ",")),
-			jwt.WithOption("deptId", user.DepartmentId),
-			jwt.WithOption("regionId", req.RegionId),
-			jwt.WithOption("positionIds", positionIds))
-		if err != nil {
-			return nil, err
-		}
-
-		// add token into database
-		expiredAt := time.Now().Add(time.Second * time.Duration(l.svcCtx.Config.Auth.AccessExpire)).UnixMilli()
-		_, err = l.svcCtx.CoreRpc.CreateToken(l.ctx, &core.TokenInfo{
-			Uuid:      user.Id,
-			Token:     pointy.GetPointer(token),
-			Source:    pointy.GetPointer("core_user"),
-			Status:    pointy.GetPointer(uint32(common.StatusNormal)),
-			Username:  user.Username,
-			ExpiredAt: pointy.GetPointer(expiredAt),
-		})
-
-		if err != nil {
-			return nil, err
-		}
-
-		err = l.svcCtx.Redis.Del(l.ctx, config.RedisCaptchaPrefix+req.CaptchaId).Err()
-		if err != nil {
-			logx.Errorw("failed to delete captcha in redis", logx.Field("detail", err))
-		}
-
-		resp = &types.LoginResp{
-			BaseDataInfo: types.BaseDataInfo{Msg: l.svcCtx.Trans.Trans(l.ctx, "login.loginSuccessTitle")},
-			Data: types.LoginInfo{
-				UserId: *user.Id,
-				Token:  token,
-				Expire: uint64(expiredAt),
-			},
-		}
-		return resp, nil
-	} else {
+	if ok := l.svcCtx.Captcha.Verify(config.RedisCaptchaPrefix+req.CaptchaId, req.Captcha, true); !ok {
 		return nil, errorx.NewCodeInvalidArgumentError("login.wrongCaptcha")
 	}
+
+	user, err := l.svcCtx.CoreRpc.GetUserByUsername(l.ctx,
+		&core.UsernameReq{
+			Username: req.Username,
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	if user.Status != nil && *user.Status != uint32(common.StatusNormal) {
+		return nil, errorx.NewCodeInvalidArgumentError("login.userBanned")
+	}
+
+	password, err := l.svcCtx.Sm2.Sm2Decrypt(req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	if !encrypt.BcryptCheck(password, *user.Password) {
+		return nil, errorx.NewCodeInvalidArgumentError("login.wrongUsernameOrPassword")
+	}
+
+	// 将整数切片转换为字符串切片
+	positionIdsSlice := make([]string, 0, len(user.PositionIds))
+	for _, num := range user.PositionIds {
+		positionIdsSlice = append(positionIdsSlice, strconv.Itoa(int(num)))
+	}
+	positionIds := strings.Join(positionIdsSlice, ",")
+
+	roleListByUser, err := l.svcCtx.CoreRpc.GetRoleList(l.ctx, &core.RoleListReq{
+		RoleIds: &core.IDsReq{
+			Ids: user.RoleIds,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	remarksMap := make(map[string]struct{})
+	for _, v := range roleListByUser.Data {
+		remarksMap[v.GetRemark()] = struct{}{}
+	}
+
+	// 拼接有权限的区域id
+	var regionIdsStr string
+	for k := range remarksMap {
+		if regionIdsStr != "" {
+			regionIdsStr += ","
+		}
+		regionIdsStr += k
+	}
+
+	token, err := jwt.NewJwtToken(
+		l.svcCtx.Config.Auth.AccessSecret, time.Now().Unix(),
+		l.svcCtx.Config.Auth.AccessExpire, jwt.WithOption("userId", user.Id),
+		jwt.WithOption("roleId", strings.Join(user.RoleCodes, ",")),
+		jwt.WithOption("deptId", user.DepartmentId),
+		jwt.WithOption("regionId", req.RegionId),
+		jwt.WithOption("positionIds", positionIds),
+		jwt.WithOption("permissionRegionIds", regionIdsStr))
+	if err != nil {
+		return nil, err
+	}
+
+	// add token into database
+	expiredAt := time.Now().Add(time.Second * time.Duration(l.svcCtx.Config.Auth.AccessExpire)).UnixMilli()
+	_, err = l.svcCtx.CoreRpc.CreateToken(l.ctx, &core.TokenInfo{
+		Uuid:      user.Id,
+		Token:     pointy.GetPointer(token),
+		Source:    pointy.GetPointer("core_user"),
+		Status:    pointy.GetPointer(uint32(common.StatusNormal)),
+		Username:  user.Username,
+		ExpiredAt: pointy.GetPointer(expiredAt),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = l.svcCtx.Redis.Del(l.ctx, config.RedisCaptchaPrefix+req.CaptchaId).Err()
+	if err != nil {
+		logx.Errorw("failed to delete captcha in redis", logx.Field("detail", err))
+	}
+
+	resp = &types.LoginResp{
+		BaseDataInfo: types.BaseDataInfo{Msg: l.svcCtx.Trans.Trans(l.ctx, "login.loginSuccessTitle")},
+		Data: types.LoginInfo{
+			UserId: *user.Id,
+			Token:  token,
+			Expire: uint64(expiredAt),
+		},
+	}
+	return resp, nil
 }
--- rpc/internal/logic/user/get_user_by_username_logic.go
+++ rpc/internal/logic/user/get_user_by_username_logic.go
@@ -2,8 +2,8 @@ package user
 
 import (
 	"context"
-
 	"github.com/suyuan32/simple-admin-common/utils/pointy"
+	"github.com/suyuan32/simple-admin-common/utils/uuidx"
 
 	"github.com/suyuan32/simple-admin-core/rpc/ent/user"
 
@@ -29,26 +29,32 @@ func NewGetUserByUsernameLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *GetUserByUsernameLogic) GetUserByUsername(in *core.UsernameReq) (*core.UserInfo, error) {
-	result, err := l.svcCtx.DB.User.Query().Where(user.UsernameEQ(in.Username)).WithRoles().First(l.ctx)
+	result, err := l.svcCtx.DB.User.Query().
+		Where(user.IDEQ(uuidx.ParseUUIDString(in.GetUsername()))).
+		WithRoles().WithPositions().WithDepartments().
+		First(l.ctx)
 	if err != nil {
 		return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
 	}
 
 	return &core.UserInfo{
-		Nickname:     &result.Nickname,
-		Avatar:       &result.Avatar,
-		Password:     &result.Password,
-		RoleIds:      GetRoleIds(result.Edges.Roles),
-		RoleCodes:    GetRoleCodes(result.Edges.Roles),
-		Mobile:       &result.Mobile,
-		Email:        &result.Email,
-		Status:       pointy.GetPointer(uint32(result.Status)),
-		Id:           pointy.GetPointer(result.ID.String()),
-		Username:     &result.Username,
-		HomePath:     &result.HomePath,
-		Description:  &result.Description,
-		DepartmentId: &result.DepartmentID,
-		CreatedAt:    pointy.GetPointer(result.CreatedAt.UnixMilli()),
-		UpdatedAt:    pointy.GetPointer(result.UpdatedAt.UnixMilli()),
+		Id:             pointy.GetPointer(result.ID.String()),
+		CreatedAt:      pointy.GetPointer(result.CreatedAt.UnixMilli()),
+		UpdatedAt:      pointy.GetPointer(result.UpdatedAt.UnixMilli()),
+		Nickname:       &result.Nickname,
+		Avatar:         &result.Avatar,
+		RoleIds:        GetRoleIds(result.Edges.Roles),
+		RoleName:       GetRoleNames(result.Edges.Roles),
+		RoleCodes:      GetRoleCodes(result.Edges.Roles),
+		PositionIds:    GetPositionIds(result.Edges.Positions),
+		Mobile:         &result.Mobile,
+		Email:          &result.Email,
+		Status:         pointy.GetPointer(uint32(result.Status)),
+		Username:       &result.Username,
+		HomePath:       &result.HomePath,
+		Password:       &result.Password,
+		Description:    &result.Description,
+		DepartmentId:   &result.DepartmentID,
+		DepartmentName: &result.Edges.Departments.Name,
 	}, nil
 }
